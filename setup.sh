b537888b6b3b66ddddc0d5c169a235949d9ad4f1
Aleksander Heintz <alxandr@alxandr.me>
2020-05-26 14:19:06 +0000

    Init

---
+++ setup.sh
@@ -0,0 +1,5 @@
+if [ ! -d "/root/.oh-my-zsh" ]; then
+  curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash -s -- --unattended
+fi
+
+curl -fsSL https://starship.rs/install.sh | bash -s -- -y
