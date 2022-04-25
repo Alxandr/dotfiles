9e85c8bb9309e8c261c3a1f29ba3553860558afa
Aleksander Heintz <alxandr@alxandr.me>
2022-04-25 09:31:33 +0200

    Remove some aliases

--- home/dot_config/shell/aliases.sh
+++ home/dot_config/shell/aliases.sh
@@ -6,11 +6,6 @@ if ls --color=auto >/dev/null 2>/dev/null; then
 fi
 alias grep='grep --color'
 
-# ls aliases
-alias ll='ls -lah'
-alias la='ls -A'
-alias l='ls'
-
 # Aliases to protect against overwriting
 alias cp='cp -i'
 alias mv='mv -i'
